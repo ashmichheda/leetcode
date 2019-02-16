99d12da59d7cc65b99aef4e9068e0f806ac81b7f
Ashmi Chheda <31774401+ashmichheda@users.noreply.github.com>
2019-02-16 17:53:59 +0530

    Palindrome Number problem

---
+++ Java/CheckPalindrome.java
@@ -0,0 +1,27 @@
+package com.prog.LeetCodeProgs;
+
+public class CheckPalindrome {
+
+	public static void main(String[] args) {
+		
+		int x = 121;
+		boolean result = isPalindrome(x);
+	}
+	
+	public static boolean isPalindrome(int x) {
+        
+		int checkPal = x, reverse = 0;
+        while(x > 0){
+        reverse = reverse*10 + x%10;
+        x = x/10;
+        } 
+        
+        if(checkPal == reverse)
+            return true;
+        else
+            return false;
+        
+        
+    }
+
+}
