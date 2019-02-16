bb4de1c7dd39d0d8f2dde3392acbaadecef37a0d
Ashmi Chheda <31774401+ashmichheda@users.noreply.github.com>
2019-02-17 00:16:30 +0530

    Problem 26: Remove duplicates from sorted array

---
+++ Problem26.java
@@ -0,0 +1,31 @@
+package com.prog.LeetCodeProgs;
+
+public class Problem26 {
+
+	public static void main(String[] args) {
+		
+		// Remove duplicates in sorted array
+		int arr[] = {0,0,1,1,1,2,2,3,3,4};
+		int length = removeDuplicates(arr);
+		
+	}
+
+	public static int removeDuplicates(int[] nums) {
+        
+		int numLen = nums.length;
+		if(numLen == 0 || numLen == 1)
+			return numLen;
+		
+		int j = 0, i;
+		for(i = 0; i<numLen - 1; i++) {
+			if(nums[i] != nums[i+1]) {
+				nums[j++] = nums[i];
+			}
+		}
+		if(i == numLen - 1) {
+			nums[j++] = nums[i];
+		}
+		
+		return j; 
+    }
+}
