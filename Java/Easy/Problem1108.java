f632d1c6bff6188defc1ab96d1fdad5ec26a7438
ashmichheda <ashmichheda@gmail.com>
2019-08-23 22:30:29 -0700

    added problem 1108

---
+++ Java/Easy/Problem1108.java
@@ -0,0 +1,15 @@
+package com.leetcodeEasy.progs;
+
+public class Problem1108 {
+
+        public String defangIPaddr(String address) {
+
+            String input[] = address.split("\\.");
+            String result = "";
+            for(int i = 0; i<input.length-1; i++){
+                result = result + input[i] + "[.]";
+            }
+            result = result + input[input.length-1];
+            return result;
+        }
+}
