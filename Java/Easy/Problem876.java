b65b922f163540fc0aba67629e36493c142d6b02
ashmichheda <ashmichheda@gmail.com>
2019-09-07 16:21:38 -0700

    added java solution for Problem 876

---
+++ Java/Easy/Problem876.java
@@ -0,0 +1,35 @@
+package com.leetcodeEasy.progs;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Problem876 {
+    public ListNode middleNode(ListNode head) {
+
+        // Add all the nodes in the list
+        List<ListNode> list = new ArrayList<ListNode>();
+        ListNode current = head;
+
+        while(current != null){
+            list.add(current);
+            current = current.next;
+        }
+        int n = list.size();
+
+        int result = 0;
+        if(n % 2 == 0)
+            result = (n/2) + 1;
+        else
+            result = (n/2) + 1;
+
+        return list.get(result-1);
+    }
+}
