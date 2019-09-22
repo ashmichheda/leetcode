69a34c101646e672ce0118ea259d344118bb5f22
ashmichheda <ashmichheda@gmail.com>
2019-09-21 17:39:24 -0700

    added java solution for Problem 136

---
+++ Java/Easy/Problem136.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+
+public class Problem136 {
+
+    public int singleNumber(int[] nums) {
+
+        Arrays.sort(nums);
+        boolean foundDuplicate = false;
+        int num = 0, length = nums.length;
+
+        if(length == 1)
+            return nums[0];
+        else{
+            for(int i = 0; i<length-1; i++){
+                if(nums[i] != nums[i+1]){
+                    if(foundDuplicate)
+                        foundDuplicate = false;
+                    else{
+                        num = nums[i];
+                        return num;
+                    }
+                }
+                else{
+                    foundDuplicate = true;
+                }
+            }
+            if(!foundDuplicate)
+                num = nums[length-1];
+            return num;
+        }
+    }
+}
