f8503fd396526b8f3dc0670e4b21216076e76215
Ashmi Chheda <ashmichheda@gmail.com>
2019-09-27 13:45:46 -0700

    added java solution for Problem 344

---
+++ Java/Easy/Problem344.java
@@ -0,0 +1,13 @@
+class Problem344 {
+    public void reverseString(char[] s) {
+        int i = 0, j = s.length-1;
+        char temp;
+        while(i < j){
+            temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
+            i++;
+            j--;
+        }
+    }
+}
