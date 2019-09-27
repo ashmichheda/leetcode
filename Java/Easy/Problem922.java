5df87f359df398710835901076ed4384910ca14e
Ashmi Chheda <ashmichheda@gmail.com>
2019-09-27 13:15:19 -0700

    updated a better approach solution for Problem 922

--- Java/Easy/Problem922.java
+++ Java/Easy/Problem922.java
@@ -1,24 +1,20 @@
 class Problem922 {
     public int[] sortArrayByParityII(int[] nums) {
         
-        int length = nums.length;
-        int len = length/2;
-        int odd[] = new int[len];
-        int even[] = new int[len];
-        int m = 0, n = 0;
-        for(int i = 0; i<length; i++){
-            if(nums[i] % 2 == 0)
-                even[m++] = nums[i];
-            else
-                odd[n++] = nums[i];
+        int result[] = new int[nums.length];
+        int evenIndex = 0; 
+        int oddIndex = 1;
+        
+        for(int i = 0; i<nums.length; i++){
+            if(nums[i] % 2 == 0){
+                result[evenIndex] = nums[i];
+                evenIndex += 2;
+            }
+            else{
+                result[oddIndex] = nums[i];
+                oddIndex += 2;
+            }
         }
-        m = 0; n = 0;
-        for(int i = 0; i<length; i++){
-            if(i % 2 == 0)
-                nums[i] = even[m++];
-            else
-                nums[i] = odd[n++];
-        }
-        return nums;
+        return result;
     }
 }
