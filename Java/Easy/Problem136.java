59eefc0d03d078cd9e1f6fbe89db0e8d76d2020c
ashmichheda <ashmichheda@gmail.com>
2020-04-01 23:57:47 -0700

    modified solution using XOR operation

--- Java/Easy/Problem136.java
+++ Java/Easy/Problem136.java
@@ -2,29 +2,12 @@ public class Problem136 {
 
     public int singleNumber(int[] nums) {
 
-        Arrays.sort(nums);
-        boolean foundDuplicate = false;
-        int num = 0, length = nums.length;
+      // Do XOR of all elements and return
+      // it is cummulative and commutative
+      int value = 0;
+      for (int ele : nums)
+          value ^= ele;
 
-        if(length == 1)
-            return nums[0];
-        else{
-            for(int i = 0; i<length-1; i++){
-                if(nums[i] != nums[i+1]){
-                    if(foundDuplicate)
-                        foundDuplicate = false;
-                    else{
-                        num = nums[i];
-                        return num;
-                    }
-                }
-                else{
-                    foundDuplicate = true;
-                }
-            }
-            if(!foundDuplicate)
-                num = nums[length-1];
-            return num;
-        }
+      return value;
     }
 }
