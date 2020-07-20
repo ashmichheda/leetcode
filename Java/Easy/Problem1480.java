7162a6b5aae95181d24c2bb98ae442e663a985ca
ashmichheda <ashmichheda@gmail.com>
2020-07-19 17:34:22 -0700

    solution for Problem 1480

---
+++ Java/Easy/Problem1480.java
@@ -0,0 +1,10 @@
+class Problem1480 {
+    public int[] runningSum(int[] nums) {
+
+        // using inplace approach
+        for(int i = 1; i<nums.length; i++){
+            nums[i] = nums[i] + nums[i-1];
+        }
+        return nums;
+    }
+}
