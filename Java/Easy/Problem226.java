ddef284ad60b312cf104518954f6c95cbbf77bd1
ashmichheda <ashmichheda@gmail.com>
2020-07-24 22:42:34 -0700

    add Problem 226

---
+++ Java/Easy/Problem226.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Problem226 {
+    public TreeNode invertTree(TreeNode root) {
+        if(root == null)
+            return null;
+        // do a pre-order traversal, left node, right node, root
+        swapNode(root);
+        return root;
+    }
+    public void swapNode(TreeNode root){
+
+        if(root == null)
+            return;
+
+        swapNode(root.left);
+        swapNode(root.right);
+
+        // swap the nodes
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+    }
+}
